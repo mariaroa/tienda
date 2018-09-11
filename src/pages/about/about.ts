2518cfaf9b151c7ae56114d138abdc0a0a77a084
Alejandra <Stiven-23@hotmail.com>
2018-09-11 11:52:37 -0500

    actualizacion

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ProductoProvider } from '../../providers/producto/producto';
+import { Producto } from '../../models/producto';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  productosTop:Producto[] =[];
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private servicioProductos: ProductoProvider )
+    {
 
+  }
+  ionViewDidLoad(){
+    this.servicioProductos.obtenerProductos().subscribe(
+      (datos: Producto[]) => {
+        this.productosTop = datos.filter(p => p.bestSeller ==true );
+      }
+    );
   }
 
 }
